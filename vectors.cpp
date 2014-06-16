49610a61e01c837477e622a1361f7aab031e2474
Pandemoneus <Pandemoneus@gmx.de>
2014-06-17 00:42:24 +0200

    Part 2 nearly done (trigonometrics still missing)

---
+++ vectors.cpp
@@ -0,0 +1,49 @@
+struct point {
+    double x, y;
+    point() {x = 0; y = 0;}
+    point(double _x, double _y) {x = _x; y = _y;}
+};
+
+struct vec {
+    double x, y;
+    vec(double _x, double _y) {x = _x; y = _y;}
+};
+
+vec toVector(point a, point b) {
+    return vec(b.x - a.x, b.y - a.y);
+}
+
+//Skalieren
+vec scale(vec v, double s) {
+    return vec(v.x * s, v.y * s);
+}
+
+//Verschieben
+point translate(point p, vec v) {
+    return point(p.x + v.x, p.y + v.y);
+}
+
+//Quadrat der euklidischen Norm
+double normSquared(vec v) {
+    return v.x * v.x + v.y * v.y;
+}
+
+//Skalarprodukt
+double dot(vec a, vec b) {
+    return (a.x * b.x + a.y * b.y);
+}
+
+//Kreuzprodukt
+double cross(vec a, vec b) {
+    return (a.x * b.y - a.y * b.x);
+}
+
+//CCW-Test
+bool ccw(point p, point q, point r) {
+    return cross(toVec(p, q), toVec(p, r)) > 0;
+}
+
+//Kollinear-Test
+bool collinear(point p, point q, point r) {
+    return fabs(cross(toVec(p, q), toVec(p, r))) < EPS;
+}
